d2e7e1b8f04d2c46757473c1851e23625abf3e67
Huynh Thi Khanh Linh <huynhthikhanhlinh2105@gmail.com>
2022-09-13 10:47:34 +0700

    site title and logo

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -3,5 +3,9 @@ export default {
     description: 'Just playing around.',
     markdown: {
         lineNumbers: true
-    }
+    },
+    themeConfig: {
+        siteTitle: 'My Custom Title',
+        logo: '/assets/logo.svg'
+    },
 }
